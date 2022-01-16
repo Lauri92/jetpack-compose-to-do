88536be3591af39ab9267e506ac3e391295b69e7
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-01-16 12:15:54 +0200

    TaskContent designed

--- app/src/main/java/fi/lauriari/to_docompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/fi/lauriari/to_docompose/ui/screens/task/TaskScreen.kt
@@ -2,6 +2,7 @@ package fi.lauriari.to_docompose.ui.screens.task
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import fi.lauriari.to_docompose.data.models.Priority
 import fi.lauriari.to_docompose.data.models.ToDoTask
 import fi.lauriari.to_docompose.util.Action
 
@@ -17,6 +18,15 @@ fun TaskScreen(
                 navigateToListScreen = navigateToListScreen
             )
         },
-        content = {}
+        content = {
+            TaskContent(
+                title = "",
+                onTitleChanged = {},
+                description = "",
+                onDescriptionChange = {},
+                priority = Priority.LOW,
+                onPrioritySelected = {}
+            )
+        }
     )
 }
