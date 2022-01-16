89cb18ba2b5e519df39e53ecb22a2f90902e7f63
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-01-16 13:21:00 +0200

    display dynamic task content

--- app/src/main/java/fi/lauriari/to_docompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/fi/lauriari/to_docompose/ui/screens/task/TaskScreen.kt
@@ -2,15 +2,22 @@ package fi.lauriari.to_docompose.ui.screens.task
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import fi.lauriari.to_docompose.data.models.Priority
 import fi.lauriari.to_docompose.data.models.ToDoTask
+import fi.lauriari.to_docompose.ui.viewmodels.SharedViewModel
 import fi.lauriari.to_docompose.util.Action
 
 @Composable
 fun TaskScreen(
     selectedTask: ToDoTask?,
+    sharedViewModel: SharedViewModel,
     navigateToListScreen: (Action) -> Unit
 ) {
+    val title: String by sharedViewModel.title
+    val description: String by sharedViewModel.description
+    val priority: Priority by sharedViewModel.priority
+
     Scaffold(
         topBar = {
             TaskAppBar(
@@ -20,12 +27,18 @@ fun TaskScreen(
         },
         content = {
             TaskContent(
-                title = "",
-                onTitleChanged = {},
-                description = "",
-                onDescriptionChange = {},
-                priority = Priority.LOW,
-                onPrioritySelected = {}
+                title = title,
+                onTitleChanged = {
+                    sharedViewModel.title.value = it
+                },
+                description = description,
+                onDescriptionChange = {
+                    sharedViewModel.description.value = it
+                },
+                priority = priority,
+                onPrioritySelected = {
+                    sharedViewModel.priority.value = it
+                }
             )
         }
     )
