be15cbbbb60604dfaed8a437099643a6c130d500
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-01-18 23:14:08 +0200

    Splashscreen designed

---
+++ app/src/main/java/fi/lauriari/to_docompose/ui/screens/splash/SplashScreen.kt
@@ -0,0 +1,63 @@
+package fi.lauriari.to_docompose.ui.screens.splash
+
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import fi.lauriari.to_docompose.R
+import fi.lauriari.to_docompose.ui.theme.LOGO_HEIGHT
+import fi.lauriari.to_docompose.ui.theme.ToDoComposeTheme
+import fi.lauriari.to_docompose.ui.theme.splashScreenBackground
+
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.splashScreenBackground),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier
+                .size(LOGO_HEIGHT),
+            painter = painterResource(id = getLogo()),
+            contentDescription = stringResource(R.string.to_do_logo)
+        )
+    }
+}
+
+@Composable
+fun getLogo(): Int {
+    return if (isSystemInDarkTheme()) {
+        R.drawable.ic_logo_dark
+    } else {
+        R.drawable.ic_logo_light
+    }
+}
+
+@Composable
+@Preview
+private fun SplashScreenPreview() {
+    SplashScreen()
+}
+
+/*
+@Composable
+@Preview
+private fun SplashScreenPreview2() {
+    ToDoComposeTheme(darkTheme = true) {
+        SplashScreen()
+    }
+}
+ */
