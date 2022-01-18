4558c4c2c3ab82d93ce2208ae184dc339bdbc203
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-01-19 00:55:20 +0200

    Splash screen animation

--- app/src/main/java/fi/lauriari/to_docompose/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/fi/lauriari/to_docompose/ui/screens/splash/SplashScreen.kt
@@ -1,20 +1,25 @@
 package fi.lauriari.to_docompose.ui.screens.splash
 
 import android.window.SplashScreen
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fi.lauriari.to_docompose.R
 import fi.lauriari.to_docompose.ui.theme.LOGO_HEIGHT
 import fi.lauriari.to_docompose.ui.theme.ToDoComposeTheme
@@ -27,7 +32,22 @@ import kotlinx.coroutines.delay
 fun SplashScreen(
     navigateToListScreen: () -> Unit
 ) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val offsetState by animateDpAsState(
+        targetValue = if (startAnimation) 0.dp else 100.dp,
+        animationSpec = tween(
+            durationMillis = 1000
+        )
+    )
+    val alphaState by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 1000
+        )
+    )
+
     LaunchedEffect(key1 = true) {
+        startAnimation = true
         delay(SPLASH_SCREEN_DELAY)
         navigateToListScreen()
     }
@@ -41,7 +61,9 @@ fun SplashScreen(
     ) {
         Image(
             modifier = Modifier
-                .size(LOGO_HEIGHT),
+                .size(LOGO_HEIGHT)
+                .offset(y = offsetState)
+                .alpha(alpha = alphaState),
             painter = painterResource(id = getLogo()),
             contentDescription = stringResource(R.string.to_do_logo)
         )
